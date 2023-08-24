8b3fb9ac9bc6de0f33787c45be7516b4fd855d8c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-08-24 16:52:42 +0200

    feat: :sparkles: rename file, add namespace(?)

--- studentportal.user.js
+++ studentportal.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Auto Scroll Greasemonkey Script
-// @namespace    http://your.namespace.com
+// @namespace    https://github.com/henrikvilhelmberglund/
 // @version      0.1
 // @author       henrikvilhelmberglund
 // @description  Automatically scrolls the page by 500px on specific URLs
 // @match        https://studentportal.nackademin.se/mod/(page|url)/view.php*
-// @downloadURL  https://raw.githubusercontent.com/henrikvilhelmberglund/tampermonkey-studentportal/main/script.js
-// @updateURL    https://raw.githubusercontent.com/henrikvilhelmberglund/tampermonkey-studentportal/main/script.js
+// @downloadURL  https://raw.githubusercontent.com/henrikvilhelmberglund/tampermonkey-studentportal/main/studentportal.user.js
+// @updateURL    https://raw.githubusercontent.com/henrikvilhelmberglund/tampermonkey-studentportal/main/studentportal.user.js
 // ==/UserScript==
 
 (function () {
