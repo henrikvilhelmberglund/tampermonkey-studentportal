8ff776014be2f604fe2122279768ecc9f0b62a69
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-08-24 16:56:56 +0200

    fix: :bug: make it work maybe

--- studentportal.user.js
+++ studentportal.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/henrikvilhelmberglund/
 // @version      0.1
 // @author       henrikvilhelmberglund
-// @description  Automatically scrolls the page by 500px on specific URLs
+// @description  Automatically scrolls the page by 386px on specific URLs
 // @match        https://studentportal.nackademin.se/mod/(page|url)/view.php*
 // @downloadURL  https://raw.githubusercontent.com/henrikvilhelmberglund/tampermonkey-studentportal/main/studentportal.user.js
 // @updateURL    https://raw.githubusercontent.com/henrikvilhelmberglund/tampermonkey-studentportal/main/studentportal.user.js
@@ -12,13 +12,18 @@
 (function () {
   "use strict";
 
-  // Function to scroll the page by a given amount of pixels
-  function scrollPageBy(amount) {
-    window.scrollBy(0, amount);
+  // Function to scroll a specific element by a given amount of pixels
+  function scrollElementBy(element, amount) {
+    element.scrollTop += amount;
   }
 
-  // Scroll the page by 500px when the page loads
-  window.onload = function () {
-    scrollPageBy(386);
-  };
+  // Find the element you want to scroll (you might need to inspect the page's HTML to get the correct selector)
+  var targetElement = document.querySelector("#page");
+
+  // Scroll the target element by 386px when the page loads
+  if (targetElement) {
+    window.onload = function () {
+      scrollElementBy(targetElement, 386);
+    };
+  }
 })();
